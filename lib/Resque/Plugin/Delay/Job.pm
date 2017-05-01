7e59c9059861dae72901bc6f2ffc113b394f3055
meru_akimbo <merukatoruayu0@gmail.com>
2017-05-01 10:01:37 +0900

    correspondence of extend payload
    SEE ALSO https://github.com/diegok/resque-perl/pull/22

--- lib/Resque/Plugin/Delay/Job.pm
+++ lib/Resque/Plugin/Delay/Job.pm
@@ -9,45 +9,14 @@ has start_time  => (
     is       => 'rw',
 );
 
-has payload => (
-    is   => 'ro',
-    isa  => 'HashRef',
-    coerce => 1,
-    lazy => 1,
-    builder => '_build_payload',
-    trigger => sub {
-        my ( $self, $hr ) = @_;
-        $self->_trigger_payload($hr);
-    },
-);
-
-
-# XXX It is difficult to extend payload with multiple plugins by overwriting payload using Moose's default or trigger as it is.
-# This plugin transfer to a dedicated method and take the form of "around" it.
-# pull request may be sent to the resque.
-sub _trigger_payload {
-    my ( $self, $hr ) = @_;
-    $self->class( $hr->{class} );
-    $self->args( $hr->{args} ) if $hr->{args};
-}
-
-sub _build_payload {
-    my ($self,) = @_;
-
-    return +{
-        class => $self->class,
-        args  => $self->args,
-    };
-}
-
-around _trigger_payload => sub {
+around payload_reader => sub {
     my ($orig, $self, $hr) = @_;
 
     $orig->($self, $hr);
     $self->start_time( $hr->{start_time} ) if $hr->{start_time};
 };
 
-around _build_payload => sub {
+around payload_builder => sub {
     my ($orig, $self) = @_;
 
     my $payload = $orig->($self);
